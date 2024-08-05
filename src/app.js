1d0187cafd90b041292c92cf7fb482bcb8b3f40e
fer6araya <162595941+fer6araya@users.noreply.github.com>
2024-08-05 02:02:26 +0000

    Generator is working with all suits and random numbers

--- src/app.js
+++ src/app.js
@@ -7,5 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["♦", "♥", "♠", "♣"];
+  let cardNumbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let suitsPicker = Math.floor(Math.random() * 4);
+  let cardNumberPicker = Math.floor(Math.random() * 13);
+
+  let selectedSuit = suits[suitsPicker];
+  let selectedCardNumber = cardNumbers[cardNumberPicker];
+
+  document.getElementById("top-left-corner-suit").innerHTML =
+    '<h5 id="top-left-corner-suit" class="card-title pinta">' +
+    selectedSuit +
+    "</h5>";
+  document.getElementById("bottom-right-corner-suit").innerHTML =
+    '<h5 id="top-left-corner-suit" class="card-title pinta">' +
+    selectedSuit +
+    "</h5>";
+  document.getElementById("card-number").innerHTML =
+    '<p id="card-number" class="card-text valor text-center">' +
+    selectedCardNumber +
+    "</p>";
 };
